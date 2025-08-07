6902961469cbc5297e63f55a79c749f7a069b094
Salcedo Chichande Micaela Stefania <mssalcedo2@espe.edu.ec>
2025-08-07 11:53:19 -0500

    diagramas actualizados

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;  
+
+//endpoint query responde un mensje
+
+app.get('/', (_req, res) => {
+  
+  res.send('Integracion continua funcionando');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor en puesrto:${PORT}`);
+});
